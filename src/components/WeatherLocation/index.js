20f13cc177195542735decff9f43a54acb91f600
Bemtorres <benja.mora.torres@gmail.com>
2020-01-08 04:36:59 -0300

    class component

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , {Component} from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData/index';
 import './styles.css';
@@ -8,13 +8,24 @@ const data = {
     weatherState: "cloud",
     humidity: 100,
     wind: '200 m/s'
-}
+};
 
-const WeatherLocation = () => (
-    <div className="weatherLocation">
-        <Location city={'Calera de Tango'}/>
-        <WeatherData props={data}/>
-    </div>
-)
+class WeatherLocation extends Component{ 
+
+    render = () => (
+        <div className="weatherLocation">
+            <Location city={'Calera de Tango'}/>
+            <WeatherData props={data}/>
+        </div>
+    );
+};
+
+// ** arrow function
+// const WeatherLocation = () => (
+//     <div className="weatherLocation">
+//         <Location city={'Calera de Tango'}/>
+//         <WeatherData props={data}/>
+//     </div>
+// )
 
 export default WeatherLocation;
