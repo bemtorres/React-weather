5564df61a3116588428f276f96f82ae39d1fa07e
Bemtorres <benja.mora.torres@gmail.com>
2020-01-09 20:38:42 -0300

    parte 2 tomando data, dia de la semana y hora

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,16 +1,42 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import './styles.css';
+import ForecastItem from './ForescastItem';
 
-
+const days  =[
+    'Lunes',
+    'Martes',
+    'Miercoles',
+    'Jueves',
+    'Viernes',
+    'Sabado',
+    'Domingo',
+];
+const data = {
+    temperature: -10,
+    weatherState: "Clouds",
+    humidity: 100,
+    wind: '200 m/s'
+};
 
 
 class ForecastExtended  extends Component {
+
+
+    renderForecastItemDays(){
+        return days.map(day => (<ForecastItem weekDay={day} hour={10} data={data}  />));
+    }
+
+
     render(){
         
         const {city} = this.props;
         
         return(
-            <div>Información Data {city}</div>
+            <div>
+                <h2 className='forecast-title'>Información Data {city}</h2>
+                {this.renderForecastItemDays()}
+            </div>
         );
     }
 }
